0088f06ddb8db57fd28201ec54d85a842bdb714a
wtaype <wilder.taype16@gmail.com>
2025-04-02 01:08:17 -0500

    Mi primer proyecto con reac vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", backgroundColor: "#87CEEB", height: "100vh", padding: "20px" }}>
+      <h1 style={{ color: "#FFD700" }}>🌊 ¡Bienvenido a Fondo de Bikini! 🦀</h1>
+      <img src="https://i.pinimg.com/originals/d0/1d/e5/d01de5340c199b550c55c16880f2851f.gif" alt="Bob Esponja Bailando" />
+      <p style={{ color: "#fff", fontSize: "20px" }}>¡Explora el mundo de Bob Esponja con React y Vite! 🧽🔥</p>
+    </div>
+  );
+}
+
+export default App;
+
